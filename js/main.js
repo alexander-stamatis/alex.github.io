fff7fd99d5271c11a04ba957a9769cace73fc710
astamatis <alex.k.stamatis@gmail.com>
2017-12-28 04:40:32 -0800

    Updated main.js logic and removed several images

--- js/main.js
+++ js/main.js
@@ -1,106 +1,129 @@
+var menuActivated = false;
+var imageFolder = "images/";
+var carouselFadeEnabled = true;
+var carouselImageDuration = 600;
+var carouselTickRate = 10;
+var carouselNoFadeImageDuration = 5000;
+var carouselOpacityCatalystRate = 0.01;
+
 $(document).ready(function() {
-    var menu_activated;
-    menu_activated = false;
-    $('#menu_icon').click(function() {
-        if (!menu_activated) {
-            this.src = "images/menu_x.png";
-            $("#nav-bar-list").show("fast");
-            menu_activated = true;
 
-        } else {
-            this.src = "images/menu.png";
-            $("#nav-bar-list").hide("fast");
-            menu_activated = false;
+  //Vertical navbar button
+  $("#menu-icon").click(function() {
+
+    if (!menuActivated) {
+      this.src = imageFolder + "menu-icon-expanded.png";
+      $("#nav-bar-list").show("fast");
+      menuActivated = true;
+    } else {
+      this.src = imageFolder + "menu-icon-contracted.png";
+      $("#nav-bar-list").hide("fast");
+      menuActivated = false;
+    }
+
+  });
+
+  //Navbar interaction
+  var pageDestination = "";
+  $("#vertical-nav-bar ul li").click(function() {
+
+    //Hiding and showing pages
+    pageDestination = "#content-" + $(this).attr("id");
+    if ($(pageDestination).is(":hidden")) {
+      $(".content-data").hide();
+      $(pageDestination).show("fast");
+    }
+
+    //Project carousels
+    var projectCarouselsActivated = false;
+    if (pageDestination == "#content-projects" && !projectCarouselsActivated) {
+      projectCarouselsActivated = false;
+      Carousel("carousel-zombie-thing", [imageFolder + "zombie-thing-menu.jpg", imageFolder + "zombie-thing-armory.jpg", imageFolder + "zombie-thing-shipyard.jpg"]);
+      Carousel("carousel-galactic-defenders", [imageFolder + "galactic-defenders-1.jpg", imageFolder + "galactic-defenders-2.jpg", imageFolder + "galactic-defenders-3.jpg", imageFolder + "galactic-defenders-4.jpg"]);
+    }
+
+  });
+
+  $("#vertical-nav-bar li").click(function() {
+
+    if (menuActivated) {
+      $("#menu-icon").attr("src", imageFolder + "menu.png");
+      $("#nav-bar-list").hide("fast");
+      menuActivated = false;
+    }
+
+  });
+
+  //Home page carousel
+  Carousel("carousel-image", [imageFolder + "zombie-thing-shipyard.jpg", imageFolder + "16_9_dark_elf.jpg", imageFolder + "galactic-defenders-2.jpg"]);
+
+  //Carousel implementation for an element
+  function Carousel(element, images) {
+
+    var imageElement = document.getElementById(element);
+
+    var currentImageIndex = {
+      item: 0
+    };
+
+    imageElement.src = images[currentImageIndex.item];
+    imageElement.style.opacity = 0;
+
+    //Fade in/out implementation
+    if (carouselFadeEnabled) {
+
+      var opacityCatalyst = 0;
+      var fadeTimer = 0;
+
+      setInterval(function() {
+
+        fadeTimer++;
+
+        if (fadeTimer > carouselImageDuration) {
+          opacityCatalyst -= carouselOpacityCatalystRate;
+          imageElement.style.opacity = opacityCatalyst;
+        } else if (imageElement.style.opacity <= 1) {
+          opacityCatalyst += carouselOpacityCatalystRate;
+          imageElement.style.opacity = opacityCatalyst;
         }
-    });
 
-    var startingPage = "home";
-    $("#vertical-nav-bar ul li").each(function(index) {
-        if ($(this).html() == startingPage.toUpperCase()) {
-            // $(this).css("color","#30BAC7");
-        } else {
-            if (!menu_activated) {
-                $(this).mouseover(function() {
-                    if (!menu_activated) {
-                        $(this).css("color", "#AE2F69");
-                    }
-                });
-                $(this).mouseout(function() {
-                    if (!menu_activated) {
-                        $(this).css("color", "black");
-                    }
-                });
-                $(this).click(function() {
-                    $(this).css("color", "#30BAC7");
-                });
-            }
+        if (fadeTimer > carouselImageDuration && imageElement.style.opacity < 0.1) {
+          imageElement.style.opacity = 0;
+          imageElement.src = NextImage(currentImageIndex, images);
+          opacityCatalyst = 0;
+          fadeTimer = 0;
         }
-    });
+      }, carouselTickRate);
 
-    var tempString = "";
-    var project_page_activated = false;
-    $('#vertical-nav-bar ul li').click(function() {
-        tempString = "#content-" + $(this).attr('id');
-        if ($(tempString).is(":visible")) {
+    } else {
+      imageElement.style.opacity = 1;
+      setInterval(function() {
+        imageElement.src = NextImage(currentImageIndex, images);
+      }, carouselNoFadeImageDuration);
 
-        } else {
-            $(".content-data").hide();
-            $(tempString).show("fast");
-        }
-        //PROJECTS PAGE
-        if (tempString == "#content-projects" && !project_page_activated) {
-            project_page_activated = true;
-			carousel('carousel-zombie-thing',false, 'images/zombie-thing-menu.jpg', 'images/zombie-thing-armory.jpg', 'images/zombie-thing-shipyard.jpg');
-            carousel('carousel-galactic-defenders',false, 'images/gd_4.png', 'images/gd_2.png', 'images/gd_3.png' );
+    }
 
-        }
-    });
+  }
 
-    var intervalHandle;
-    var language_bar_max_width = 100;
+  //Cycle through image array by reference
+  function NextImage(index, images) {
+    index.item = index.item < images.length - 1 ? ++index.item : 0;
+    return images[index.item];
+  }
 
-    // Percentages
-    var skill_c_sharp = 80;
-    var skill_javascript = 60;
-    var skill_html = 55;
-    var skill_php = 20;
-    var skill_c_plus = 60;
-    var delay_tween = 0.5;
-    var rate = 3;
+  //Load sketchfab plugin
+  document.getElementById("sketchfab-container").style.display = "none";
+  document.getElementById("img-blacksmith").addEventListener("click", LoadSketchfabPlugin);
+  document.getElementById("img-blacksmith").style.opacity = 1;
 
-    $('li#bio').click(function() {
-        $('.progress-bar').css('width', '0');
-        TweenLite.to($('#c-sharp-bar'), rate, {
-            width: skill_c_sharp * 2,
-            delay: delay_tween
-        });
-        TweenLite.to($('#javascript-bar'), rate, {
-            width: skill_javascript * 2,
-            delay: delay_tween
-        });
-        TweenLite.to($('#html-bar'), rate, {
-            width: skill_html * 2,
-            delay: delay_tween
-        });
-        TweenLite.to($('#php-bar'), rate, {
-            width: skill_php * 2,
-            delay: delay_tween
-        });
-        TweenLite.to($('#c-plus-bar'), rate, {
-            width: skill_c_plus * 2,
-            delay: delay_tween
-        });
-    });
+  //Load external sketchfab plugin
+  function LoadSketchfabPlugin() {
+    document.getElementById("img-blacksmith").style.display = "none";
+    document.getElementById("sketchfab-container").style.display = "block";
+    document.getElementById("iframe-blacksmith").src = "https://sketchfab.com/models/6e7f2a177287411ebd8920c0087406b7/embed";
+  }
 
-    $('#vertical-nav-bar li').click(function() {
-        if (menu_activated) {
-            $('#menu_icon').attr('src', 'images/menu.png');
-            $("#nav-bar-list").hide("fast");
-            menu_activated = false;
-        }
-    });
-
-    $(".all-images").click(function() {
+  $(".all-images").click(function() {
         $("#magic-box").show();
         var image_location = $(this).attr("src");
         $("#magic-box-image").attr({
@@ -111,90 +134,5 @@ $(document).ready(function() {
     $("#magic-box-exit").click(function() {
         $(this).parent().hide();
     });
-
-    //CAROUSEL
-    function carousel(target_element, fade_enabled, image_1, image_2, image_3) {
-        var img = document.getElementById(target_element);
-        var images = [];
-        var seconds = 10;
-        var image_index = 0;
-        var duration = 800;
-        var rate_of_fade = .10; //percentage
-        var delta_fade_in = (25 / 100) * duration;
-        var delta_fade_out = (75 / 100) * duration;
-        images[0] = image_1;
-        images[1] = image_2;
-        images[2] = image_3;
-
-        img.src = images[0];
-        img.style.opacity = 0;
-
-        function ChangeImage() {
-            if (image_index < images.length - 1) {
-                image_index++;
-            } else {
-                image_index = 0;
-            }
-            img.src = images[image_index];
-
-        }
-
-        if(fade_enabled){
-            var adder = 0;
-            var timer = 0;
-
-            function Fade() {
-                timer++;
-                if (timer > delta_fade_out) {
-                    adder -= .01;
-                    img.style.opacity = adder;
-                } else {
-                    if (img.style.opacity <= 1) {
-                        adder += .01;
-                        img.style.opacity = adder;
-                    }
-                }
-                if (timer > delta_fade_out && img.style.opacity < .1) {
-                    console.debug(image_index);
-                    img.style.opacity = 0;
-                    adder = 0;
-                    timer = 0;
-                    ChangeImage();
-                }
-            }
-            //repeat carousel
-            setInterval(function() {
-                Fade();
-            }, seconds);
-        } else{
-            img.style.opacity = 1;
-            setInterval(function() {
-                ChangeImage();
-            }, 5000);
-        }
-    }
-    //HOME PAGE
-    carousel('carousel-image', true, 'images/zombie-thing-shipyard.jpg', 'images/16_9_dark_elf.jpg', 'images/gd_4.jpg');
-    
-    
-    document.getElementById("sketchfab-container").style.display = 'none';
-    document.getElementById("img-blacksmith").addEventListener("click",loadBlacksmith);
-    document.getElementById("img-blacksmith").style.opacity = 1;
-    function loadBlacksmith(){
-        document.getElementById("img-blacksmith").style.display = 'none';
-        document.getElementById("sketchfab-container").style.display = 'block';
-        document.getElementById("iframe-blacksmith").src = "https://sketchfab.com/models/7cbe284b3e9d4087b2473edd982cd089/embed";
-    }
-
-    document.getElementById("soundcloud-container").style.display = 'none';
-    document.getElementById("soundcloud-link").addEventListener("click", loadSoundcloud);
-    document.getElementById("soundcloud-link").style.cursor = 'pointer';
-    function loadSoundcloud(){
-        document.getElementById("soundcloud-link").style.display = 'none';
-        document.getElementById("soundcloud-container").style.display = 'block';
-        document.getElementById("soundcloud-iframe").src = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/192289403&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false';
-    }
-
-
 
 });
