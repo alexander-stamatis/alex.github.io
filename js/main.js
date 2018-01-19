f0b14b9d4688713d7b5944b46b2720e3c0c98508
astamatis <alex.k.stamatis@gmail.com>
2018-01-18 22:20:59 -0800

    fixing files not found in new look

--- js/main.js
+++ js/main.js
@@ -1,62 +1,55 @@
-var menuActivated = false;
 var imageFolder = "images/";
-var imageMenuIconContracted = imageFolder + "menu-icon-contracted.png";
 var carouselFadeEnabled = true;
 var carouselImageDuration = 600;
 var carouselTickRate = 10;
 var carouselNoFadeImageDuration = 5000;
 var carouselOpacityCatalystRate = 0.01;
+var resumeLink = "https://drive.google.com/open?id=1NQZ-bIqQANbjilaW1EN8XDfTbgm8DbDz";
 
 $(document).ready(function() {
 
-  //Vertical navbar button
-  $("#menu-icon").click(function() {
+  $(".resume-links").attr("href", resumeLink);
 
-    if (!menuActivated) {
-      this.src = imageFolder + "menu-icon-expanded.png";
-      $("#nav-bar-list").show("fast");
-      menuActivated = true;
-    } else {
-      this.src = imageMenuIconContracted;
-      $("#nav-bar-list").hide("fast");
-      menuActivated = false;
-    }
-
+  //mobile nav bar
+  $("#menu-icon-contracted").click(function() {
+      $("#nav-bar").css("display", "block");
+      $("#menu-icon-expanded").css("display", "block");
+      $(this).css("display", "none");
   });
+
+  function DeactivateMobileNavigation(){
+    $("#nav-bar").css("display", "none");
+    $("#menu-icon-contracted").css("display", "block");
+    $("#menu-icon-expanded").css("display", "none");
+  }
+
+  $("#menu-icon-expanded").click(function() {
+    DeactivateMobileNavigation();
+  });
+
 
   //Navbar interaction
   var pageDestination = "";
-  $("#vertical-nav-bar ul li").click(function() {
+  $("#nav-bar ul li").click(function() {
 
     //Hiding and showing pages
     pageDestination = "#content-" + $(this).attr("id");
     if ($(pageDestination).is(":hidden")) {
       $(".content-data").hide();
-      $(pageDestination).show("fast");
+      $(pageDestination).show();
     }
+
+    DeactivateMobileNavigation();
 
     //Project carousels
     var projectCarouselsActivated = false;
     if (pageDestination == "#content-projects" && !projectCarouselsActivated) {
       projectCarouselsActivated = false;
-      Carousel("carousel-zombie-thing", [imageFolder + "zombie-thing-menu.jpg", imageFolder + "zombie-thing-armory.jpg", imageFolder + "zombie-thing-shipyard.jpg"]);
-      Carousel("carousel-galactic-defenders", [imageFolder + "galactic-defenders-1.jpg", imageFolder + "galactic-defenders-2.jpg", imageFolder + "galactic-defenders-3.jpg", imageFolder + "galactic-defenders-4.jpg"]);
     }
-
-  });
-
-  $("#vertical-nav-bar li").click(function() {
-
-    if (menuActivated) {
-      $("#menu-icon").attr("src", imageMenuIconContracted);
-      $("#nav-bar-list").hide("fast");
-      menuActivated = false;
-    }
-
   });
 
   //Home page carousel
-  Carousel("carousel-image", [imageFolder + "zombie-thing-shipyard.jpg", imageFolder + "16_9_dark_elf.jpg", imageFolder + "galactic-defenders-2.jpg"]);
+  Carousel("carousel-home-img", ["zombie-thing-shipyard-2.jpg", "mss-md.jpg", "darkness.jpg", "16_9_dark_elf.jpg"]);
 
   //Carousel implementation for an element
   function Carousel(element, images) {
@@ -67,7 +60,7 @@ $(document).ready(function() {
       item: 0
     };
 
-    imageElement.src = images[currentImageIndex.item];
+    imageElement.style.backgroundImage = "url(" + imageFolder + images[currentImageIndex.item] + ")";
     imageElement.style.opacity = 0;
 
     //Fade in/out implementation
@@ -90,7 +83,7 @@ $(document).ready(function() {
 
         if (fadeTimer > carouselImageDuration && imageElement.style.opacity < 0.1) {
           imageElement.style.opacity = 0;
-          imageElement.src = NextImage(currentImageIndex, images);
+          imageElement.style.backgroundImage = "url(" + imageFolder + NextImage(currentImageIndex, images) + ")";
           opacityCatalyst = 0;
           fadeTimer = 0;
         }
@@ -99,7 +92,7 @@ $(document).ready(function() {
     } else {
       imageElement.style.opacity = 1;
       setInterval(function() {
-        imageElement.src = NextImage(currentImageIndex, images);
+        imageElement.style.backgroundImage = "url(" + imageFolder + NextImage(currentImageIndex, images) + ")";
       }, carouselNoFadeImageDuration);
 
     }
@@ -109,9 +102,11 @@ $(document).ready(function() {
   //Cycle through image array by reference
   function NextImage(index, images) {
     index.item = index.item < images.length - 1 ? ++index.item : 0;
+    console.log(index.item);
     return images[index.item];
   }
 
+  //Enlarging and closing images
   $(".all-images").click(function() {
         $("#magic-box").show();
         var image_location = $(this).attr("src");
@@ -122,6 +117,19 @@ $(document).ready(function() {
 
     $("#magic-box-exit").click(function() {
         $(this).parent().hide();
+    });
+
+
+    // $("h2.intro-home").css("visibility")
+    $("h2.intro-home").addClass('animated fadeInLeft');
+    $("h2.intro-home").css("visibility", "visible");
+    $("h2.intro-home").animate('fadeInLeft', function() {
+      $("p.intro-home").css("visibility", "visible");
+      $("p.intro-home").addClass('animated fadeInRight');
+      $("p.intro-home").animate('animated fadeInRight', function() {
+        $(".resume-links").css("visibility", "visible");
+        $(".resume-links").addClass('animated bounceInDown');
+      });
     });
 
 });
